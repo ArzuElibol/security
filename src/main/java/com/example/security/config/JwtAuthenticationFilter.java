ad2f9f461c9c4a8e50ba4f4c381b10c07c2ae52b
arzu <arzu@elibol.com>
2023-08-11 14:15:06 +0300

    proje son

--- src/main/java/com/example/security/config/JwtAuthenticationFilter.java
+++ src/main/java/com/example/security/config/JwtAuthenticationFilter.java
@@ -2,6 +2,7 @@ package com.example.security.config;
 
 import java.io.IOException;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.lang.NonNull;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -20,8 +21,9 @@ import lombok.RequiredArgsConstructor;
 @Component
 @RequiredArgsConstructor
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
-
+    @Autowired
     private final JwtService jwtService;
+    @Autowired
     private UserDetailsService userDetailsService;
 
     @Override
--- src/main/java/com/example/security/demo/DemoController.java
+++ src/main/java/com/example/security/demo/DemoController.java
@@ -2,6 +2,7 @@ package com.example.security.demo;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
